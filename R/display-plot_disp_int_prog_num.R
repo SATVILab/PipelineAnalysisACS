e6912a1c54a752927d5b08101d47bd2bc37f4861
Miguel <rdxmig002@myuct.ac.za>
2021-07-29 15:53:34 +0200

    Debug plot_disp_int_prog_num

--- R/display-plot_disp_int_prog_num.R
+++ R/display-plot_disp_int_prog_num.R
@@ -11,12 +11,6 @@ plot_disp_int_cat_num <- function(mod, .data, data_nm,
   # prep
   # --------------
 
-  # get non-progressor variable name
-  # var_p_n <- setdiff(var_int, "Progressor")
-  #var_alt_ind <- which(colnames(eff_tbl) == np_var)
-  #colnames(eff_tbl)[var_alt_ind] <- 'var_alt'
-
-
   # get estimates and ci's
   eff_obj <- run_effects_Effect(
     data = .data,
@@ -25,7 +19,7 @@ plot_disp_int_cat_num <- function(mod, .data, data_nm,
     mod = mod
   )
 
-  eff_tbl <- eff_obj %>%
+  plot_tbl_eff <- eff_obj %>%
     as.data.frame() %>%
     tibble::as_tibble()
 
@@ -40,38 +34,108 @@ plot_disp_int_cat_num <- function(mod, .data, data_nm,
   # is number of cells
   if(n_cell_ind){
     n_cell <- exp(eff_obj$offset)
-    eff_tbl <- eff_tbl %>%
+    plot_tbl_eff <- plot_tbl_eff %>%
       dplyr::mutate(fit = fit / n_cell * 1e2,
                     lower = lower / n_cell * 1e2,
                     upper = upper / n_cell * 1e2)
   }
 
-  # calculate plot bounds
-  # -----------------------
+  # prep data_raw
+  # -------------------
+
+  plot_tbl_raw <- .data
+
+  # account for offset, if need be
+  if (!is.null(var_offset)) {
+    plot_tbl_raw[, var_dep] <- plot_tbl_raw[, var_dep] / plot_tbl_raw[, var_offset]
+    if (n_cell_ind) plot_tbl_raw[, var_dep] <- plot_tbl_raw[, var_dep] * 1e2
+  }
+
+  # restrict plot_tbl_eff to where
+  # we actually have data
+  # ----------------------
+
+  if (!(var_cat == "Progressor" && var_num == "tfmttb")) {
+
+    cat_vec <- unique(plot_tbl_eff[[var_cat]]) %>%
+      as.character()
+
+    plot_tbl_eff <- purrr::map_df(cat_vec, function(cat) {
+      eff_tbl_filter <- plot_tbl_eff %>%
+        dplyr::filter(.data[[var_cat]] == .env$cat)
+      raw_tbl_filter <- plot_tbl_raw %>%
+        dplyr::filter(.data[[var_cat]] == .env$cat)
+      pad_var_num <- diff(range(raw_tbl_filter[[var_num]],
+                                na.rm = TRUE)) * 0.01
+      lims_tbl_raw <- raw_tbl_filter %>%
+        dplyr::summarise(min = min(.data[[var_num]], na.rm = TRUE) -
+                           pad_var_num,
+                         max = max(.data[[var_num]], na.rm = TRUE) +
+                           pad_var_num)
+
+      eff_tbl_filter_max <- eff_tbl_filter %>%
+        dplyr::filter(.data[[var_num]] >= lims_tbl_raw$max)
+      if (nrow(eff_tbl_filter_max) > 0) {
+        max_num_var <- eff_tbl_filter_max %>%
+          dplyr::filter(.data[[var_num]] == min(.data[[var_num]],
+                                                na.rm = TRUE)) %>%
+          dplyr::pull(var_num)
+        max_num_var <- min(max_num_var)
+      } else {
+        max_num_var <- max(eff_tbl_filter[[var_num]])
+      }
+
+      eff_tbl_filter_min <- eff_tbl_filter %>%
+        dplyr::filter(.data[[var_num]] <= lims_tbl_raw$min)
+      if (nrow(eff_tbl_filter_min) > 0) {
+        min_num_var <- eff_tbl_filter_min %>%
+          dplyr::filter(.data[[var_num]] == max(.data[[var_num]],
+                                                na.rm = TRUE)) %>%
+          dplyr::pull(var_num)
+        min_num_var <- max(min_num_var)
+      } else {
+        min_num_var <- min(eff_tbl_filter[[var_num]])
+      }
+      eff_tbl_filter %>%
+        dplyr::filter(.data[[var_num]] >= min_num_var,
+                      .data[[var_num]] <= max_num_var)
+    })
+  }
+
+
+  # recalibrate tfmttb
+  # ------------------
+
+  if (var_cat == "Progressor" && var_num == "tfmttb") {
+    ttb_max <- rlang::caller_env()$p_dots$iter$ttb_max
+
+    plot_tbl_raw <- plot_tbl_raw %>%
+      dplyr::mutate(tfmttb = ttb_max  - (tfmttb * 1e2))
+  }
 
   # stretch non-progressor estimates
   # across range of numeric variable
   if (var_cat == "Progressor" && var_num == "tfmttb") {
-    eff_tbl_prog <- eff_tbl %>%
+    eff_tbl_prog <- plot_tbl_eff %>%
       dplyr::filter(Progressor == 'yes')
 
     eff_tbl_ctrl <- purrr::map_df(eff_tbl_prog$tfmttb, function(x){
-      eff_tbl %>%
+      plot_tbl_eff %>%
         dplyr::filter(Progressor == 'no',
                       tfmttb == 0) %>%
         dplyr::mutate(tfmttb = .env$x)
     })
 
-    eff_tbl <- eff_tbl_prog %>%
+    plot_tbl_eff <- eff_tbl_prog %>%
       dplyr::bind_rows(eff_tbl_ctrl)
 
     ttb_max <- rlang::caller_env()$p_dots$iter$ttb_max
 
-    eff_tbl <- eff_tbl %>%
+    plot_tbl_eff <- plot_tbl_eff %>%
       dplyr::mutate(tfmttb = ttb_max  - (tfmttb * 1e2))
   }
 
-  p <- ggplot(eff_tbl, aes(x = .data[[var_num]], y = fit,
+  p <- ggplot(plot_tbl_eff, aes(x = .data[[var_num]], y = fit,
                       col = .data[[var_cat]],
                       fill = .data[[var_cat]])) +
     cowplot::background_grid(major = 'xy', minor = 'y') +
@@ -108,20 +172,6 @@ plot_disp_int_cat_num <- function(mod, .data, data_nm,
 
   # format raw data
 
-  plot_tbl_raw <- .data
-
-  # account for offset, if need be
-  if (!is.null(var_offset)) {
-    plot_tbl_raw[, var_dep] <- plot_tbl_raw[, var_dep] / plot_tbl_raw[, var_offset]
-    if (n_cell_ind) plot_tbl_raw[, var_dep] <- plot_tbl_raw[, var_dep] * 1e2
-  }
-
-  if (var_cat == "Progressor" && var_num == "tfmttb") {
-    ttb_max <- rlang::caller_env()$p_dots$iter$ttb_max
-
-    plot_tbl_raw <- plot_tbl_raw %>%
-      dplyr::mutate(tfmttb = ttb_max  - (tfmttb * 1e2))
-  }
 
   p_raw <- p +
     geom_point(data = plot_tbl_raw,
@@ -130,12 +180,24 @@ plot_disp_int_cat_num <- function(mod, .data, data_nm,
   # restrict to between 0 and 100 output if modelling frequencies
   # ------------------
   if (n_cell_ind) {
-    max_pt <- max(plot_tbl_raw$resp) + 0.01 * diff(range(plot_tbl_raw$resp))
-    max_est <- quantile(eff_tbl$upper, 0.75)
-    max_val <- min(100, max_pt, max_est)
+    max_pt <- max(plot_tbl_raw$resp)
+    max_est <- quantile(plot_tbl_eff$upper, 0.75)
+    max_val <- max(max_pt, max_est)
     p <- p + coord_cartesian(ylim = c(0, max_val))
     p_raw <- p_raw +
       coord_cartesian(ylim = c(0, max_val))
+  } else {
+    max_pt <- max(plot_tbl_raw$resp)
+    max_est <- quantile(plot_tbl_eff$upper, 0.75)
+    max_val <- max(max_pt, max_est)
+
+    min_pt <- min(plot_tbl_raw$resp)
+    min_est <- quantile(plot_tbl_eff$lower, 0.25)
+    min_val <- min(min_pt, min_est)
+
+    p <- p + coord_cartesian(ylim = c(min_val, max_val))
+    p_raw <- p_raw +
+      coord_cartesian(ylim = c(min_val, max_val))
   }
 
   p_list <- list(p, p_raw)
@@ -150,7 +212,10 @@ plot_disp_int_cat_num <- function(mod, .data, data_nm,
       dplyr::mutate(var = purrr::map_chr(var, function(x){
         x %>%
           stringr::str_replace("; ", " and/or ") %>%
-          stringr::str_replace(":",  " int. with ")
+          stringr::str_replace(":",  " int. with ") %>%
+          stringr::str_replace("or tfmttb",  "or time") %>%
+          stringr::str_replace("tfmttb",  "Time") %>%
+          stringr::str_replace("risk6",  "RISK6")
       }))
 
     res_text <- paste0(test_tbl$var, ": ", signif(test_tbl$p, digits = 3))
